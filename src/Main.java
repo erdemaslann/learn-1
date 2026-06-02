c51cfe8bef93e8c5c11794d0091912fb61efb4db
Erdem Aslan <erdemaslan173@gmail.com>
2026-06-02 17:15:19 +0300

    Koleksiyonlar: List, Map, Set ornekleri ve kelime sayaci

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("github deneme 2");
+
+        String metin = "elma armut elma kirz armut elma";
+
+        // 1. Metni boşluklardan kelimelere böl
+        String[] kelimeler = metin.split(" ");
+
+        // 2. Sayacı tutacak Map
+        Map<String, Integer> sayac = new HashMap<>();
+
+        // 3. Her kelimeyi dolaş
+        for (String kelime : kelimeler) {
+            if (sayac.containsKey(kelime)) {
+                // varsa: mevcut sayıyı al, 1 ekle, geri koy
+                sayac.put(kelime, sayac.get(kelime) + 1);
+            } else {
+                // yoksa: ilk kez görüyoruz, 1 yaz
+                sayac.put(kelime, 1);
+            }
+        }
+
+        // 4. Sonucu yazdır
+        for (Map.Entry<String, Integer> entry : sayac.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
